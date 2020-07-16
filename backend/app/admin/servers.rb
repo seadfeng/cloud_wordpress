45d616cea3f2a7c1b851c7a2aef3d51b13cd336b
side Feng <seadfeng@gmail.com>
2020-07-16 22:16:19 +0800

    update locales yml

--- backend/app/admin/servers.rb
+++ backend/app/admin/servers.rb
@@ -61,12 +61,12 @@ end
     index do
         selectable_column
         id_column
-        column :cloudflare
-        column :name
-        column :description 
         column :cname do |source|
             source.domain
         end
+        column :cloudflare
+        column :name
+        column :description  
         column :host 
         column :install do |source|
             if source.installed
@@ -103,20 +103,20 @@ end
     form do |f|
         f.inputs I18n.t("active_admin.php_service.form" , default: "服务器")  do  
           f.input :cloudflare, label: "Cloudflare"  
-          f.input :domain, placeholder: "host1.demo.com"  , label: "主机CName地址", hint: "用处:博客cloudflare二级域名指向此地址"  
+          f.input :domain, placeholder: "host1.demo.com" , hint: "用处:博客cloudflare二级域名指向此地址"  
           f.input :name, label: "服务器名字"  
           f.input :description 
           f.input :max_size, label: I18n.t("active_admin.php_service.max_size" , default: "博客最大数量") 
-          f.input :host, placeholder: "127.0.0.1"  , label: "主机IP地址"   
-          f.input :host_port, placeholder: "22" , label: "主机端口"   
-	      f.input :host_user, placeholder: "root" , label: "主机账户名"  
-          f.input :host_password , placeholder: "password" , label: "主机账户密码" , hint: "密码保存后不显示"    
+          f.input :host, placeholder: "127.0.0.1"  
+          f.input :host_port, placeholder: "22" 
+	      f.input :host_user, placeholder: "root"  
+          f.input :host_password , placeholder: "password"  , hint: "密码保存后不显示"    
           hr
-          f.input :mysql_host, placeholder: "192.168.10.10"  , label: "Mysql主机地址"   
-          f.input :mysql_host_user, placeholder: "192.168.%.%"  , label: "Mysql账户主机地址" , hint: "root@#{f.object.mysql_host_user.blank? ? "127.0.0.1" : f.object.mysql_host_user}"      
-          f.input :mysql_port, placeholder: "3306" , label: "Mysql端口"   
-	      f.input :mysql_user, placeholder: "root" , label: "Mysql账户名"  
-          f.input :mysql_password , placeholder: "password" , label: "Mysql账户密码" , hint: "密码保存后不显示"     
+          f.input :mysql_host, placeholder: "192.168.10.10"  
+          f.input :mysql_host_user, placeholder: "192.168.%.%"  , hint: "root@#{f.object.mysql_host_user.blank? ? "127.0.0.1" : f.object.mysql_host_user}"      
+          f.input :mysql_port, placeholder: "3306"   
+	      f.input :mysql_user, placeholder: "root" 
+          f.input :mysql_password , placeholder: "password"  , hint: "密码保存后不显示"     
           f.input :installed   
         end
         f.actions
