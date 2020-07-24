f001303a2bdca8b2b07c54a9df7cc68dacda419d
side Feng <seadfeng@gmail.com>
2020-07-24 16:07:18 +0800

    update cloudflare

--- backend/app/admin/cloudflares.rb
+++ backend/app/admin/cloudflares.rb
@@ -6,6 +6,14 @@ ActiveAdmin.register Wordpress::Cloudflare,  as: "Cloudflare" do
     permit_params  :api_user, :name, :api_token ,:description , :domain
     active_admin_paranoia
 
+
+    controller do
+        def update  
+            params[:cloudflare][:api_token] = resource.api_token if params[:cloudflare][:api_token].blank?
+            super 
+        end
+    end
+
     index do
         selectable_column
         id_column
@@ -26,10 +34,10 @@ ActiveAdmin.register Wordpress::Cloudflare,  as: "Cloudflare" do
     
     form do |f|
         f.inputs I18n.t("active_admin.cloudflare.form" , default: "Cloudflare")  do  
-            f.input :name  
-            f.input :domain  
-            f.input :api_user   
-            f.input :api_token     
+            f.input :name, hint: "根据自己命名习惯起名"    
+            f.input :domain, hint: "Cloudflare必须有解析权限的域名，用于博客的二级域名"  
+            f.input :api_user , hint: "Cloudflare登陆账户名"    
+            f.input :api_token , as: :password , hint: raw("Cloudflare Api Key => Global API Key => View<br /><a href=\"https://dash.cloudflare.com/profile/api-tokens\" target=\"_blank\">快捷链接<a/>" )      
             f.input :description    
         end
         f.actions
