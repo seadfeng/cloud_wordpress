2aada64ebb8a521f62189a1ad69457d3da8065c1
side Feng <seadfeng@gmail.com>
2020-07-25 15:44:11 +0800

    update server job

--- core/app/models/concerns/wordpress/routeable.rb
+++ core/app/models/concerns/wordpress/routeable.rb
@@ -3,13 +3,13 @@ module Wordpress
         extend ActiveSupport::Concern
     
         included do
-            include Rails.application.routes.url_helpers
+            include Wordpress::Core::Engine.routes.url_helpers
         end
 
-        # protected
+        protected
     
-        # def default_url_options
-        #     Rails.application.config.action_mailer.default_url_options
-        # end
+        def default_url_options
+            Rails.application.routes.default_url_options || {}
+        end
     end
 end
