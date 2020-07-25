2aafcd5973752d55881daef703f75f6de2dd07d5
side Feng <seadfeng@gmail.com>
2020-07-25 17:25:34 +0800

    Update install code

--- backend/app/admin/templates.rb
+++ backend/app/admin/templates.rb
@@ -102,7 +102,7 @@ ActiveAdmin.register Wordpress::Template,  as: "Template" do
           f.input :install_url, placeholder: "https://wordpress.org/latest.tar.gz" , label: "安装地址"        
           f.input :name  
         #   f.input :mysql_user , placeholder: "user" 
-          f.input :mysql_password , placeholder: "password" if current_admin_user.admin? 
+          # f.input :mysql_password , placeholder: "password" if current_admin_user.admin? 
         #   f.input :wordpress_user , placeholder: "admin" 
         #   f.input :wordpress_password , placeholder: "password" 
           f.input :description, label: "备注"   
