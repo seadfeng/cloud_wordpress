894664376718aad4b3bb61049dede27fc8571f37
side Feng <seadfeng@gmail.com>
2020-07-27 14:10:03 +0800

    update mysql2

--- core/lib/wordpress/core/helpers/mysql2.rb
+++ core/lib/wordpress/core/helpers/mysql2.rb
@@ -10,8 +10,27 @@ module Wordpress
                     @connect = connect 
                 end   
                 
+                def blogname
+                    option_find_by_name('blogname')
+                end
+
+                def permalink  
+                    option_find_by_name('permalink_structure')
+                end
+
+                def blogname=(name)
+                    return nil if name.blank?
+                    update_wp_option('blogname', name)
+                end
+
+                def permalink=(common = "/%postname%/")
+                    return nil if option_name.blank?
+                    update_wp_option('permalink_structure', common )
+                end
+
                 def blog_update_siteurl(new_url)
-                    client.query("update wp_options set option_value='#{new_url}' where option_name='siteurl' or option_name='home'")
+                    update_wp_option('siteurl', new_url )
+                    update_wp_option('home', new_url ) 
                 end
 
                 def blog_update_admin_password( *userinfo )
@@ -20,7 +39,46 @@ module Wordpress
                     password = userinfo["password"] 
                     username = userinfo["username"] || "admin"
                     client.query("update wp_users set user_pass=md5(\"#{password}\"), user_login=\"#{username}\", user_nicename=\"#{username}\" where id=1;")
+                end 
+
+                def option_find_by_name(option_name)
+                    return nil if option_name.blank?
+                    statement = client.prepare("SELECT * FROM wp_options WHERE option_name = ?")
+                    statement.execute(option_name)&.first
                 end
+
+                def post_find_by_id(id) 
+                    statement = client.prepare("SELECT * FROM wp_posts WHERE id = ?")
+                    statement.execute(option_name)&.first
+                end
+
+                def post_status_count(statu)
+                    where_and_count(post_status: statu)
+                end
+
+                def post_status(statu)
+                    where(post_status: statu)
+                end
+
+                def where(*options) 
+                    options = options.first || {}
+                    sql = options.map { |key,val| "#{key} = '#{vale}'" }.join(" and ")  
+                    client.query("SELECT * FROM wp_posts WHERE #{sql}") 
+                end 
+
+                def where_and_count(*options) 
+                    options = options.first || {}
+                    sql = options.map { |key,val| "#{key} = '#{vale}'" }.join(" and ")  
+                    client.query("SELECT count( id ) as count FROM wp_posts WHERE #{sql}") 
+                end 
+
+                #post_status
+
+                def update_wp_option(option_name, option_value)
+                    return nil if option_name.blank?
+                    statement = client.prepare("update wp_options set option_value = ? WHERE option_name = ?")
+                    statement.execute(option_value, option_name)
+                end 
 
                 def create_mysql_user(*userinfo)
                     userinfo = userinfo.first || {}  
