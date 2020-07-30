70fa0de4a79b597c6dcc56c21b358364b2bc7dce
side Feng <seadfeng@gmail.com>
2020-07-30 15:40:29 +0800

    Update

--- core/app/models/wordpress/monitor.rb
+++ core/app/models/wordpress/monitor.rb
@@ -1,12 +1,6 @@
 module Wordpress
   class Monitor < Wordpress::Base
-    belongs_to :resource, polymorphic: true  
+    belongs_to :blog  
 
-    def resource_class_name
-      # resource.is_a?(Wordpress::Blog)
-      self.resource_type
-    end
-
-    
   end
 end
--- core/db/migrate/20200730071621_create_wordpress_monitors.rb
+++ core/db/migrate/20200730071621_create_wordpress_monitors.rb
@@ -1,7 +1,7 @@
 class CreateWordpressMonitors < ActiveRecord::Migration[6.0]
   def change
     create_table :wordpress_monitors do |t|
-      t.references :resource, polymorphic: true
+      t.belongs_to :blog 
       t.string     :state 
       t.string     :action
       t.datetime   :queued_at
